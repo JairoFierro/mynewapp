06ac538d352a60080faf9fb00b486ee90045b975
Jairo <ja.fierro@uniandes.edu.co>
2024-05-05 17:20:47 -0500

    TallerAngular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './encabezado/encabezado.component';
+import {SerieComponent} from './serie/serie.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [	
+    AppComponent,
+      EncabezadoComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule
